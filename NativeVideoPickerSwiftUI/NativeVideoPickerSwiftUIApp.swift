d1bf38ec26064522c342e7bf2bec7438c5f43f75
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-06-27 12:09:00 +0300

    Initial Commit

---
+++ NativeVideoPickerSwiftUI/NativeVideoPickerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NativeVideoPickerSwiftUIApp.swift
+//  NativeVideoPickerSwiftUI
+//
+//  Created by Yunus Emre Taşdemir on 27.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct NativeVideoPickerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
